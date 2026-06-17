f91c2173ac7afd3f6057d7acce775b7b12823177
BadzeiPavel <pavelbadzei@gmail.com>
2026-06-17 19:26:08 +0300

    move public paths into configuration properties

--- src/main/java/com/innowise/gateway/config/security/SecurityConfig.java
+++ src/main/java/com/innowise/gateway/config/security/SecurityConfig.java
@@ -1,33 +1,35 @@
 package com.innowise.gateway.config.security;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-  @Value("${security.public-paths}")
-  private List<String> publicPaths;
+  private final SecurityProperties securityProperties;
+
+  public SecurityConfig(SecurityProperties securityProperties) {
+    this.securityProperties = securityProperties;
+  }
 
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
       ReactiveJwtTokenFilter jwtTokenFilter) {
+    String[] publicPathsArray = securityProperties.getPublicPaths().toArray(new String[0]);
+
     http
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
         .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
         .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-        .authorizeExchange(exchanges -> {
-          AuthorizeExchangeSpec registry = exchanges.pathMatchers(publicPaths.toArray(new String[0])).permitAll();
-          registry.anyExchange().authenticated();
-        })
+        .authorizeExchange(exchanges -> exchanges
+            .pathMatchers(publicPathsArray).permitAll()
+            .anyExchange().authenticated()
+        )
         .addFilterAt(jwtTokenFilter, SecurityWebFiltersOrder.AUTHENTICATION);
 
     return http.build();
